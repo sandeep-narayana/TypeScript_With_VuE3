0dff794da3098a4080756d6390d214285c8b41d5
Sandeep Rawat <132456591+sandeep-narayana@users.noreply.github.com>
2023-08-28 18:37:31 +0530

    basic use of typescript with vue 3

--- typescript_vue_demo/vue_typescript_project/src/App.vue
+++ typescript_vue_demo/vue_typescript_project/src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
   <div class="app">
-    Hello
+    <p>{{ this.name }} {{ age }}</p>
+    <button v-on:click="changeName('zelda')">Change Name</button>
+     <button v-on:click="changeAge(55)">Change Name</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-  }
+  name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Sandeep",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: number) {
+      this.age = age;
+      return age;
+    },
+  },
 });
 </script>
 
 <style>
-
 </style>
